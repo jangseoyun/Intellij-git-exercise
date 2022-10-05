3ba81783a4c3a69ee1a9f78642ec7b48d2006fb9
seoyun <may-primavera@naver.com>
2022-10-05 10:19:53 +0900

    commit: 코드 추가

--- src/codelion/java/week3/CalculatorMain.java
+++ src/codelion/java/week3/CalculatorMain.java
@@ -3,5 +3,9 @@ package codelion.java.week3;
 public class CalculatorMain {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        calculator.plus(10, 20);
+        calculator.minus(20, 10);
+        calculator.multiple(10, 20);
+        calculator.divide(20, 2);
     }
 }
