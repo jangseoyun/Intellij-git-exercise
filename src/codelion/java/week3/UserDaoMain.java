2519462b69adbc9dd52b92c4c14bc9ecfee6cf09
seoyun <may-primavera@naver.com>
2022-10-05 10:19:41 +0900

    commit: 코드 추가

---
+++ src/codelion/java/week3/UserDaoMain.java
@@ -0,0 +1,8 @@
+package codelion.java.week3;
+
+public class UserDaoMain {
+    public static void main(String[] args) {
+        UserDao userDao = new UserDao();
+        userDao.plus(10, 20);
+    }
+}
