88f3fdebb1d85ec59426ced4f58d59dea83cee7a
seoyun <may-primavera@naver.com>
2022-10-05 11:31:25 +0900

    refact: 랜덤번호 클래스 생성자를 사용하여 생성

--- src/codelion/java/week3/RandomCalculator.java
+++ src/codelion/java/week3/RandomCalculator.java
@@ -7,27 +7,24 @@ public class RandomCalculator {
 
     public RandomCalculator(int input) {
         this.input = input;
+        this.randomMachine = new RandomMachine();
     }
 
     public void plus() {
-        System.out.println(input + new RandomMachine().randomNum());
+        System.out.println(input + randomMachine.randomNum());
     }
 
     public void minus() {
-        System.out.println(input - new RandomMachine().randomNum());
+        System.out.println(input - randomMachine.randomNum());
     }
 
     public void multiple() {
-        System.out.println(input * new RandomMachine().randomNum());
+        System.out.println(input * randomMachine.randomNum());
     }
 
     public void divide() {
-        try {
-            System.out.println((float) input / new RandomMachine().randomNum());
-        } catch (ArithmeticException e) {
-            System.out.println("랜덤하게 생성된 숫자가 0입니다");
-        }
-
+        //0은 randomMachine에서 처리
+        System.out.println((float) input / randomMachine.randomNum());
     }
 
 
--- src/codelion/java/week3/RandomMachine.java
+++ src/codelion/java/week3/RandomMachine.java
@@ -3,8 +3,21 @@ package codelion.java.week3;
 import java.util.Random;
 
 public class RandomMachine {
+
+    private int randomNum;
+
+    public RandomMachine() {
+        this.randomNum = randomNum();
+    }
+
     public int randomNum() {
         Random random = new Random();
-        return random.nextInt(9);
+        int result = random.nextInt(9);
+
+        if (result == 0) {
+            System.out.println("랜덤하게 생성된 숫자가 0입니다");
+        }
+
+        return result;
     }
 }
