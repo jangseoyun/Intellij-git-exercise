da17e088fe57cf04d796b4d646f923ba46ad63d6
seoyun <may-primavera@naver.com>
2022-10-05 10:16:41 +0900

    feat: plus minus multiple divide 메서드 생성 후 main에서 호출

---
+++ src/codelion/java/week3/Calculator.java
@@ -0,0 +1,20 @@
+package codelion.java.week3;
+
+public class Calculator {
+    //plus minus multiple divide
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multiple(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/codelion/java/week3/CalculatorMain.java
@@ -0,0 +1,7 @@
+package codelion.java.week3;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+    }
+}
