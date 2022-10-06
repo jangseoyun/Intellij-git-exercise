400cbdfeb0b79615b891c8a6b2119376eae2e349
seoyun <may-primavera@naver.com>
2022-10-06 09:32:03 +0900

    feat: 리스트로 학생 명단 출력하기

---
+++ src/codelion/java/cellection/ListExercise.java
@@ -0,0 +1,20 @@
+package codelion.java.cellection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExercise {
+    private List<String> students;
+
+    public ListExercise() {
+        this.students = new ArrayList<>();
+        this.students.add("김경록");
+        this.students.add("김민지");
+        this.students.add("김미지");
+        this.students.add("김민경");
+    }
+
+    public List<String> getStudents() {
+        return this.students;
+    }
+}
---
+++ src/codelion/java/cellection/ListExerciseMain.java
@@ -0,0 +1,16 @@
+package codelion.java.cellection;
+
+import java.util.List;
+
+public class ListExerciseMain {
+    public static void main(String[] args) {
+        ListExercise listExercise = new ListExercise();
+
+        List<String> students = listExercise.getStudents();
+        for (String student : listExercise.getStudents()) {
+            System.out.println(student);
+        }
+
+        System.out.println(students.size());
+    }
+}
