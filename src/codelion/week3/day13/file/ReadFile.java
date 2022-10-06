78179a4c73a61723e1650f87f6e542c78ee7c396
seoyun <may-primavera@naver.com>
2022-10-06 15:37:44 +0900

    refact: 파일 한글자, 두글자, 한줄 읽어오는 메서드

--- src/codelion/week3/day13/file/ReadFile.java
+++ src/codelion/week3/day13/file/ReadFile.java
@@ -15,34 +15,43 @@ public class ReadFile {
         this.filename = filename;
     }
 
-    char readAByte(String filename) {
+    public char readAByte(String filename) {
         try (BufferedReader br = Files.newBufferedReader(//buffer로 파일 읽기
-            Paths.get("a_file.txt"), StandardCharsets.UTF_8)){//파일 경로에 관한 정보 가져오기,
-            return (char)br.read();
+                Paths.get("a_file.txt"), StandardCharsets.UTF_8)) {//파일 경로에 관한 정보 가져오기,
+            return (char) br.read();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    /*String readTwoByte(String filename) {
+    public String readTwoWord(String filename) throws IOException {
+        FileReader fileReader = new FileReader(filename);
+        String str = "";
+        str += (char) fileReader.read();
+        str += (char) fileReader.read();
+        return str;
+    }
 
-    }*/
-
-    String readLine(String filename) {
+    public String readLine(String filename) {
         try (BufferedReader br = Files.newBufferedReader(//buffer로 파일 읽기
-                Paths.get("a_file.txt"), StandardCharsets.UTF_8)){//파일 경로에 관한 정보 가져오기,
+                Paths.get("a_file.txt"), StandardCharsets.UTF_8)) {//파일 경로에 관한 정보 가져오기,
             return br.readLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException{
         ReadFile readFile = new ReadFile("a_file.txt");//읽을 파일명 변수에 저장
         char c = readFile.readAByte("./a_file.txt");//메서드 호출 -> 한 바이트 가져오기
         System.out.println(c);
 
         String str = readFile.readLine("./a_file.txt");
         System.out.println(str);
+
+        String ch2 = readFile.readTwoWord("./a_file.txt");
+        System.out.println(ch2);
     }
 }
+
