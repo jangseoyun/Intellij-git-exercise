7e33c718451d17419d0baa579a8d8c483c9d9351
seoyun <may-primavera@naver.com>
2022-10-06 14:24:36 +0900

    feat: File 한바이트씩 읽어오기

---
+++ src/codelion/week3/day13/file/ReadFile.java
@@ -0,0 +1,32 @@
+package codelion.week3.day13.file;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadFile {
+
+    private String filename;
+
+    public ReadFile(String filename) {
+        this.filename = filename;
+    }
+
+    char readAByte(String filename) {
+        try (BufferedReader br = Files.newBufferedReader(
+            Paths.get("a_file.txt"), StandardCharsets.UTF_8)){
+            return (char)br.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+        ReadFile readFile = new ReadFile("a_file.txt");
+        char c = readFile.readAByte("./a_file.txt");
+        System.out.println(c);
+    }
+}
