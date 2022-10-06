533956a67b0a048c6e982ef06e49816cfe023b02
seoyun <may-primavera@naver.com>
2022-10-06 15:03:10 +0900

    feat: 파일 한 라인씩 읽어오는 메서드 추가

--- src/codelion/week3/day13/file/ReadFile.java
+++ src/codelion/week3/day13/file/ReadFile.java
@@ -1,6 +1,7 @@
 package codelion.week3.day13.file;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -15,16 +16,33 @@ public class ReadFile {
     }
 
     char readAByte(String filename) {
-        try (BufferedReader br = Files.newBufferedReader(
-            Paths.get("a_file.txt"), StandardCharsets.UTF_8)){
+        try (BufferedReader br = Files.newBufferedReader(//buffer로 파일 읽기
+            Paths.get("a_file.txt"), StandardCharsets.UTF_8)){//파일 경로에 관한 정보 가져오기,
             return (char)br.read();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+    /*String readTwoByte(String filename) {
+
+    }*/
+
+    String readLine(String filename) {
+        try (BufferedReader br = Files.newBufferedReader(//buffer로 파일 읽기
+                Paths.get("a_file.txt"), StandardCharsets.UTF_8)){//파일 경로에 관한 정보 가져오기,
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     public static void main(String[] args) {
-        ReadFile readFile = new ReadFile("a_file.txt");
-        char c = readFile.readAByte("./a_file.txt");
+        ReadFile readFile = new ReadFile("a_file.txt");//읽을 파일명 변수에 저장
+        char c = readFile.readAByte("./a_file.txt");//메서드 호출 -> 한 바이트 가져오기
         System.out.println(c);
+
+        String str = readFile.readLine("./a_file.txt");
+        System.out.println(str);
     }
 }
