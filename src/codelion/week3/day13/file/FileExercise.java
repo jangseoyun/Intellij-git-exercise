dd63b23c6d55b38dff097ec2528dcdac14d00623
seoyun <may-primavera@naver.com>
2022-10-06 15:29:35 +0900

    feat: 파일 한글자씩 읽어오기

--- src/codelion/week3/day13/file/FileExercise.java
+++ src/codelion/week3/day13/file/FileExercise.java
@@ -3,15 +3,26 @@ package codelion.week3.day13.file;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class FileExercise {
-    public static void main(String[] args) throws FileNotFoundException {
-        FileReader fileReader = new FileReader("./a_file.txt");
-        File dir = new File("./");
+    public void printFiles() throws FileNotFoundException {// 현재 디렉토리 확인
+        File dir = new File("home:\\");
         File[] files = dir.listFiles();
         for (File file : files) {
             System.out.println(file);
         }
+    }
+
+    public char readAChar(String filename) throws IOException {// 한글자 읽어오기
+        FileReader fileReader = new FileReader(filename);
+        return (char) fileReader.read();
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileExercise fileExercise = new FileExercise();
+        char ch = fileExercise.readAChar("a_file.txt");
+        System.out.println(ch);
     }
 
 }
