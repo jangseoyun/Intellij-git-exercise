9389e46d5fa3ffa246f719b1262392d8b894d5bd
seoyun <may-primavera@naver.com>
2022-10-06 10:19:11 +0900

    commit: 클래스 제거

--- src/codelion/week3/day13/set/RanNumbersWithoutDuplicated.java
+++ src/codelion/week3/day13/set/RanNumbersWithoutDuplicated.java
@@ -2,15 +2,20 @@ package codelion.week3.day13.set;
 
 import codelion.week3.day12.calculator.RandomNumberGenerator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RanNumbersWithoutDuplicated {
     public static void main(String[] args) {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-        RandomNumber randomNumber = new RandomNumber();
+        Set<Integer> setNumber = new HashSet<>();
+
         for (int i = 0; i < 50; i++) {
             int r = randomNumberGenerator.generate(10);//10미만의 숫자 생성
+            setNumber.add(r);
             System.out.println(r);
-            randomNumber.setNumber(r);
         }
+        System.out.println(setNumber);
 
     }
 }
