6c7f8fcff4596fe6d9e014c5cb41dae63e265052
seoyun <may-primavera@naver.com>
2022-10-07 10:42:33 +0900

    feat: 메타 데이터 읽은 후 데이터 분석
    - 서울에서 가장 많이 이사간 지역

---
+++ src/codelion/week3/day14/file/PopulationMove.java
@@ -0,0 +1,20 @@
+package codelion.week3.day14.file;
+
+public class PopulationMove {
+    private int fromSido;
+    private int toSido;
+
+    public PopulationMove(int fromSido, int toSido) {
+        this.fromSido = fromSido;
+        this.toSido = toSido;
+    }
+
+    public int getFromSido() {
+        return fromSido;
+    }
+
+    public int getToSido() {
+        return toSido;
+    }
+
+}
--- src/codelion/week3/day14/file/PopulationStatistice.java
+++ src/codelion/week3/day14/file/PopulationStatistice.java
@@ -1,15 +1,28 @@
 package codelion.week3.day14.file;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class PopulationStatistice {
-    public void readFileByLine() {
-
+    public void readByLine2(String filename) {
+        try (BufferedReader br = Files.newBufferedReader(
+                Paths.get(filename), StandardCharsets.UTF_8
+        )) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
-    public static void main(String[] args) throws IOException {
-        String address = "/Users/seoyun/codeLion/2021_인구관련연간자료_20221007_32002.csv";
-        FileReader fileReader = new FileReader(address);
+
+    public void readByChar(String filename) throws IOException {//1byte 읽는 기능
+        FileReader fileReader = new FileReader(filename);
 
         String fileContents = "";
         while (fileContents.length() < 1_000_000) {
@@ -18,5 +31,30 @@ public class PopulationStatistice {
         }
 
         System.out.println(fileContents);
+    }
+
+    public void readByLine(String filename) throws IOException {//한줄씩 읽는 기능
+        BufferedReader br = new BufferedReader(
+                new FileReader(filename)
+        );
+        String str;
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+
+        br.close();
+    }
+
+    public PopulationMove parse(String data) {//한줄 읽어옴
+        String[] splitLine = data.split(",");
+        Integer fromSido = Integer.valueOf(splitLine[0]);//0번 전입 , 6번 전출
+        Integer toSido = Integer.valueOf(splitLine[6]);
+        return new PopulationMove(fromSido, toSido);
+    }
+
+    public static void main(String[] args) throws IOException {
+        String address = "/Users/seoyun/codeLion/2021_인구관련연간자료_20221007_32002.csv";
+        PopulationStatistice ps = new PopulationStatistice();
+        ps.readByLine2(address);
     }
 }
