9a63d9fe5231cc2b58703901d35c53700f0841c7
seoyun <may-primavera@naver.com>
2022-10-13 10:10:20 +0900

    feat: 알고리즘 - 버블정렬

--- src/codelion/week4/day17/algorithm/Algorithm01.java
+++ src/codelion/week4/day17/algorithm/Algorithm01.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Algorithm01 {
     public int[] sort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
                     int tmp = arr[i];
                     arr[i] = arr[j];
