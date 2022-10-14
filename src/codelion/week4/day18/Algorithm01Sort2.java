a9837a848878ef5a53abada94c7ee2bc2fd54b3f
seoyun <may-primavera@naver.com>
2022-10-14 10:26:22 +0900

    feat: 삽입정렬 알고리즘

--- src/codelion/week4/day18/Algorithm01Sort2.java
+++ src/codelion/week4/day18/Algorithm01Sort2.java
@@ -4,15 +4,18 @@ import java.util.Arrays;
 
 public class Algorithm01Sort2 {//삽입 정렬 알고리즘
 
-    public int[] solution(int[] arr, int i) {
+    public int[] solution(int[] arr) {
 
-        int j = i;
-        if (arr[j] < arr[j - 1]) {
-            int tmp = arr[j - 1];
-            arr[j - 1] = arr[j];
-            arr[j] = tmp;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                //System.out.printf("i:%d j:%d j-1:%d\n", i, j, j-1);
+                if (arr[j] < arr[j - 1]) {
+                    int tmp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
         }
-        System.out.println(Arrays.toString(arr));
 
         return arr;
     }
@@ -20,6 +23,6 @@ public class Algorithm01Sort2 {//삽입 정렬 알고리즘
     public static void main(String[] args) {
         Algorithm01Sort2 T = new Algorithm01Sort2();
         int[] arr = {8, 5, 6, 2, 4};
-        System.out.println(T.solution(arr, 2));
+        System.out.println(Arrays.toString(T.solution(arr)));
     }
 }
