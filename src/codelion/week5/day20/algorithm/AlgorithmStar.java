b25e88227faa82abaf04e2c4cfceb3173a618268
seoyun <may-primavera@naver.com>
2022-10-18 09:46:34 +0900

    feat: 별찍기 (재귀)

--- src/codelion/week5/day20/algorithm/AlgorithmStar.java
+++ src/codelion/week5/day20/algorithm/AlgorithmStar.java
@@ -1,16 +1,19 @@
 package codelion.week5.day20.algorithm;
 
+import java.util.Scanner;
+
 public class AlgorithmStar {
     public void printStar(int n, String now) {
-        if (n == now.length()) {
-            System.out.println("종료");
+        if (n < now.length()) {
             return;
         }
         System.out.print(now + "\n");
-        printStar(10, now + "*");
+        printStar(n, now + "*");
     }
 
     public static void main(String[] args) {
-        new AlgorithmStar().printStar(5, "*");
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        new AlgorithmStar().printStar(input, "*");
     }
 }
